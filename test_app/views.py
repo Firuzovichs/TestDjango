0c76f1c5a87b263c6ea653763fae8b84ea2a15f3
Firuzovichs <firuzovichs@gmail.com>
2025-02-27 18:25:58 +0500

    25ta

--- test_app/views.py
+++ test_app/views.py
@@ -11,8 +11,8 @@ class RandomTestQuestionsAPIView(APIView):
         questions = list(TestQuestion.objects.all())
 
         # Agar 30 ta yoki undan ko‘p savol bo‘lsa, tasodifiy 30 tasini tanlab olamiz
-        if len(questions) > 30:
-            questions = random.sample(questions, 30)
+        if len(questions) > 25:
+            questions = random.sample(questions, 25)
 
         # Serializatsiya qilish
         serializer = TestQuestionSerializer(questions, many=True)
